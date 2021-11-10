32a591163a3263317364e72ef6d62bd0b11e8469
Ahmed Nouira <ahmnouira@gmail.com>
2021-11-10 23:57:39 +0100

    complelte conversion

--- src/App.tsx
+++ src/App.tsx
@@ -4,43 +4,34 @@ import { WalletCard } from './components/WalletCard'
 import { WalletRow } from './components/WalletRow'
 import { WalletChip } from './components/WalletChip'
 import React from 'react'
-import { getLatestRates } from './api/getCurrency'
 import { CurrencyName } from './types/currency'
 import { convertCurrency } from './api/convertCurrency'
 
 function App() {
 
   // this represent the user selected currencies
-  const [currencies, setCurrencies] = React.useState<CurrencyName[]>(["EUR", "GBP", "USD"])
-  const [rates, setRates] = React.useState({})
+  const [currencies, setCurrencies] = React.useState<CurrencyName[]>(["EUR", "GBP"])
+  const [rate, setRate] = React.useState<number | null>(null)
+  const [loading, setLoading] = React.useState<boolean>(false)
 
   React.useEffect(() => {
     const getData = async () => {
-      const result = await getLatestRates("EUR", ["EUR", "GBP", "USD"])
-      if (!result.success) {
-        return
-      }
-      setRates(result.rates)
+      setLoading(true)
+      const result = await convertCurrency(currencies[0], currencies[1])
+      setRate(result[`${currencies[0]}_${currencies[1]}`] as number)
     }
-
-
     getData()
-  }, [])
+    setLoading(false)
+  }, [currencies])
 
 
   const handleChange = async (currency: CurrencyName, idx: number) => {
-
-
     const updatedCurrencies = [...currencies]
     updatedCurrencies[idx] = currency
-
-    console.log(updatedCurrencies)
-    const result = await convertCurrency(currency, updatedCurrencies[idx])
-    console.log(Object.values(result)[0])
     setCurrencies(updatedCurrencies)
   }
 
-  if (JSON.stringify(rates) === '{}') {
+  if (rate === null) {
     return (
       <div className='app'>
         <Container>
@@ -57,13 +48,11 @@ function App() {
       <Container>
         <Typography variant="h5" color='gray'>Currency Exchange Proptype</Typography>
         <WalletCard>
-          <WalletRow currencies={['EUR', 'GBP']} onChange={handleChange} index={0} />
+          <WalletRow currencies={['EUR', 'GBP', "USD"]} onChange={handleChange} index={0} />
           <Box style={{ width: '100%', position: 'relative', backgroundColor: '#dfe6e9' }}>
-            <WalletChip position='top' from={currencies[0]} to={currencies[1]} value={rates[currencies[1]]} />
-            <WalletRow currencies={['GBP', 'USD']} onChange={handleChange} index={1} />
-            <WalletChip position='bottom' from={currencies[1]} to={currencies[2]} value={rates[currencies[2]]} />
+            <WalletChip position="top" from={currencies[0]} to={currencies[1]} value={rate} loading={loading} />
+            <WalletRow currencies={['GBP', 'USD', "EUR"]} onChange={handleChange} index={1} />
           </Box>
-          <WalletRow currencies={['USD', 'EUR']} onChange={handleChange} index={2} />
         </WalletCard>
         <Button color='secondary' variant='contained' onClick={() => { }}>
           Exchange
--- src/components/WalletChip.tsx
+++ src/components/WalletChip.tsx
@@ -3,21 +3,23 @@ import { getCurrency } from '../helpers/getCurrency'
 import { CurrencyName } from '../types/currency'
 
 type WalletChipProps = {
-  position: 'top' | 'bottom'
   from: CurrencyName
   to: CurrencyName
-  value: string
+  value: number
+  loading: boolean
+  position?: 'top' | 'bottom'
+
 }
 
-export const WalletChip = ({ position, from, to, value }: WalletChipProps) => {
+export const WalletChip = ({ from, to, value, loading, position = "top" }: WalletChipProps) => {
 
   const renderLabel = () => {
-    return `1${getCurrency(from)}=${value}${getCurrency(to)}`
+    return value ? `1${getCurrency(from)}=${value}${getCurrency(to)}` : `try after`
   }
 
   return (
     <Box style={{ position: 'absolute', [position]: -16, right: '50%' }} mr={2} ml={2}>
-      <Chip label={renderLabel()} color='success' variant='outlined' style={{ backgroundColor: 'white', minWidth: 120 }} />
+      <Chip label={renderLabel()} color={value ? "success" : "error"} variant='outlined' style={{ backgroundColor: 'white', minWidth: 120 }} />
     </Box>
   )
 }
